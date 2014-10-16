ce49e85b4d8a86eb5865b30528eebad004f2fe31
Phil <phil@parsons.uk.com>
2014-10-17 01:35:08 +0200

    added script to setup database

--- wordpress.sh
+++ wordpress.sh
@@ -2,8 +2,25 @@
 
 cd /var/www
 sudo chown phil: .
-rm *
+sudo rm -r *
 wget http://wordpress.org/latest.tar.gz
 tar xzf latest.tar.gz
 mv wordpress/* .
 rm -rf wordpress latest.tar.gz
+
+EXPECTED_ARGS=1
+E_BADARGS=65
+MYSQL=`which mysql`
+ 
+Q1="CREATE DATABASE IF NOT EXISTS wordpress;"
+Q2="GRANT ALL ON *.* TO 'wordpress'@'localhost' IDENTIFIED BY '$1';"
+Q3="FLUSH PRIVILEGES;"
+SQL="${Q1}${Q2}${Q3}"
+ 
+if [ $# -ne $EXPECTED_ARGS ]
+then
+  echo "Usage: $0 dbpass"
+  exit $E_BADARGS
+fi
+ 
+$MYSQL -uroot -p -e "$SQL"
