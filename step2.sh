af18c14778e77f14c25a7e5f6963411bd4b455bd
Phil <phil@parsons.uk.com>
2014-10-17 00:55:23 +0200

    created a 3rd step

--- step2.sh
+++ step2.sh
@@ -19,22 +19,5 @@ sudo apt-get install emacs -y
 sudo apt-get install apache2 -y
 sudo apt-get install mysql -y
 
-#Switch to Phil and link in dotfiles
+#Switch to Phil
 su phil
-if [ ! -f /home/phil/raspberrypi_config ]; then
-	git clone https://github.com/pcp135/raspberrypi_config.git
-	ln -sb raspberrypi_config/.screenrc .
-fi
-
-#Identify with github
-git config --global user.name "Phil Parsons"
-git config --global user.email phil@parsons.uk.com
-
-if [ ! -f /home/phil/.ssh/id_rsa.pub ]; then
-
-	# Generate new key with no passphrase
-	ssh-keygen -t rsa -f ~/.ssh/id_rsa -N ''
-
-	# Output rsa key to screen for copying into git
-	cat ~/.ssh/id_rsa.pub
-fi
