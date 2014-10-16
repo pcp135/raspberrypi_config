2afc5a3c5b92b6a1a41c258c9ff3bc629c373db2
Phil <phil@parsons.uk.com>
2014-10-17 01:37:02 +0200

    changed username to argument

--- wordpress.sh
+++ wordpress.sh
@@ -8,18 +8,18 @@ tar xzf latest.tar.gz
 mv wordpress/* .
 rm -rf wordpress latest.tar.gz
 
-EXPECTED_ARGS=1
+EXPECTED_ARGS=2
 E_BADARGS=65
 MYSQL=`which mysql`
  
 Q1="CREATE DATABASE IF NOT EXISTS wordpress;"
-Q2="GRANT ALL ON *.* TO 'wordpress'@'localhost' IDENTIFIED BY '$1';"
+Q2="GRANT ALL ON *.* TO '$1'@'localhost' IDENTIFIED BY '$2';"
 Q3="FLUSH PRIVILEGES;"
 SQL="${Q1}${Q2}${Q3}"
  
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage: $0 dbpass"
+  echo "Usage: $0 dbuser dbpass"
   exit $E_BADARGS
 fi
  
