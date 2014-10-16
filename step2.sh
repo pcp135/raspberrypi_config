69d8dfe9d09a1f2216c997b08f5d6a7f1a5a7ba2
Phil <phil@parsons.uk.com>
2014-10-17 01:25:05 +0200

    moved install jobs into the step2

--- step2.sh
+++ step2.sh
@@ -16,7 +16,8 @@ fi
 # Install apps
 sudo apt-get install screen -y
 sudo apt-get install emacs -y
-sudo apt-get install apache2 -y
+sudo apt-get install php5 libapache2-mod-php5 -y
+sudo apt-get install mysql-server php5-mysql -y
 
 #Switch to Phil
 su phil
