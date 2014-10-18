912e5abc2af7903a9bfee62d8e7ef5596feeb553
Phil <phil@parsons.uk.com>
2014-10-18 12:08:05 +0200

    fixed script to actually check checksums

--- owncloud.sh
+++ owncloud.sh
@@ -4,5 +4,13 @@ if [ ! -f owncloud-7.0.2.tar.bz2 ]; then
 	wget https://download.owncloud.org/community/owncloud-7.0.2.tar.bz2
 fi
 wget https://download.owncloud.org/community/owncloud-7.0.2.tar.bz2.md5
-md5sum owncloud-7.0.2.tar.bz2
-cat owncloud-7.0.2.tar.bz2.md5
+$file1 = `cut -d" " -f1 owncloud-7.0.2.tar.bz2.md5`
+$file2 = `md5sum owncloud-7.0.2.tar.bz2 | cut -d" " -f1`
+
+if [ $file1 != $file2 ]
+then
+  echo "md5 sums mismatch"
+	exit
+else
+  echo "checksums OK"
+fi
