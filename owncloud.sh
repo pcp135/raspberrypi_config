1d4490db60c181a8604adb1b61b04f341db2a92c
Phil <phil@parsons.uk.com>
2014-10-18 12:09:08 +0200

    fixed spacing

--- owncloud.sh
+++ owncloud.sh
@@ -4,8 +4,8 @@ if [ ! -f owncloud-7.0.2.tar.bz2 ]; then
 	wget https://download.owncloud.org/community/owncloud-7.0.2.tar.bz2
 fi
 wget https://download.owncloud.org/community/owncloud-7.0.2.tar.bz2.md5
-$file1 = `cut -d" " -f1 owncloud-7.0.2.tar.bz2.md5`
-$file2 = `md5sum owncloud-7.0.2.tar.bz2 | cut -d" " -f1`
+$file1=`cut -d" " -f1 owncloud-7.0.2.tar.bz2.md5`
+$file2=`md5sum owncloud-7.0.2.tar.bz2 | cut -d" " -f1`
 
 if [ $file1 != $file2 ]
 then
