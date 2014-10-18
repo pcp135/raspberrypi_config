8baf0f7153626f9d4010fdf333f52e7fb440616d
Phil Parsons <phil@parsons.uk.com>
2014-10-18 10:36:43 +0000

    improvements to script

--- owncloud.sh
+++ owncloud.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+mkdir /var/www/owncloud
+cd /var/www/owncloud
 if [ ! -f owncloud-7.0.2.tar.bz2 ]; then
 	wget https://download.owncloud.org/community/owncloud-7.0.2.tar.bz2
 fi
 wget https://download.owncloud.org/community/owncloud-7.0.2.tar.bz2.md5
-$file1=`cut -d" " -f1 owncloud-7.0.2.tar.bz2.md5`
-$file2=`md5sum owncloud-7.0.2.tar.bz2 | cut -d" " -f1`
+wget https://download.owncloud.org/community/owncloud-7.0.2.tar.bz2.asc
+wget https://owncloud.org/owncloud.asc
+file1=`cut -d" " -f1 owncloud-7.0.2.tar.bz2.md5`
+file2=`md5sum owncloud-7.0.2.tar.bz2 | cut -d" " -f1`
 
 if [ $file1 != $file2 ]
 then
   echo "md5 sums mismatch"
 	exit
 else
-  echo "checksums OK"
+  echo "md5 checksum OK"
 fi
+
+echo "GPG verification output"
+gpg --import owncloud.asc
+gpg --verify owncloud-7.0.2.tar.bz2.asc owncloud-7.0.2.tar.bz2
