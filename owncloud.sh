1ba19edad57bc85f401395a2538d23c5fe97afd8
Phil <phil@parsons.uk.com>
2014-10-18 11:49:20 +0200

    added md5 checking

--- owncloud.sh
+++ owncloud.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-wget https://download.owncloud.org/community/owncloud-7.0.2.tar.bz2
+if [ ! -f owncloud-7.0.2.tar.bz2 ]; then
+	wget https://download.owncloud.org/community/owncloud-7.0.2.tar.bz2
+fi
 wget https://download.owncloud.org/community/owncloud-7.0.2.tar.bz2.md5
+md5sum owncloud-7.0.2.tar.bz2
+cat owncloud-7.0.2.tar.bz2.md5
