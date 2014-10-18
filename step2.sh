2a38f2066d549047d1157b791f6ecce6931c56cb
Phil <phil@parsons.uk.com>
2014-10-18 13:46:04 +0200

    modified to use lighttpd instead of apache

--- step2.sh
+++ step2.sh
@@ -8,6 +8,7 @@ sudo apt-get upgrade -y
 sudo adduser phil
 sudo adduser phil pi 
 sudo adduser phil sudo
+sudo adduser phil www-data
 if [ ! -f /home/phil/authorized_keys ]; then
 	sudo mkdir /home/phil/.ssh
 	sudo touch /home/phil/.ssh/authorized_keys
@@ -16,8 +17,11 @@ fi
 # Install apps
 sudo apt-get install screen -y
 sudo apt-get install emacs -y
-sudo apt-get install php5 libapache2-mod-php5 -y
+sudo apt-get install lightppd -y
+sudo apt-get install php5-common php5-cgi php5 -y
 sudo apt-get install mysql-server php5-mysql -y
+sudo lighty-enable-mod fastcgi-php
+sudo service lighttpd force-reload
 
 #Switch to Phil
 su phil
