f15affacc9bc2617c4237959f4379406ccbd5083
Phil Parsons <phil@parsons.uk.com>
2016-07-03 11:53:11 +0200

    added sudo for copy of webserver config

--- webserver.sh
+++ webserver.sh
@@ -8,7 +8,7 @@ sudo lighty-enable-mod fastcgi-php
 sudo service lighttpd force-reload
 
 #Set permissions on the web path
-cp $HOME/raspberrypi_config/.htaccess /var/www/
+sudo cp $HOME/raspberrypi_config/.htaccess /var/www/
 cd /var/www
 sudo chown phil: .
 sudo rm -r *
