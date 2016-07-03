fd18f8bf7b37a89d9b73fbb5f97ec8a0ed778215
Phil Parsons <phil@parsons.uk.com>
2016-07-03 11:43:02 +0200

    changed roles on new key files and fixed a typo in step2

--- step2.sh
+++ step2.sh
@@ -6,12 +6,14 @@ sudo apt-get upgrade -y
 
 # Setup new user
 sudo adduser phil
-sudo adduser phil pi 
+sudo adduser phil pi
 sudo adduser phil sudo
 sudo adduser phil www-data
 if [ ! -f /home/phil/authorized_keys ]; then
 	sudo mkdir /home/phil/.ssh
 	sudo touch /home/phil/.ssh/authorized_keys
+	sudo chown -R phil /home/phil/.ssh
+	sudo chgrp -R phil /home/phil/.ssh
 fi
 
 # Install apps
@@ -20,7 +22,7 @@ sudo apt-get install emacs -y
 sudo apt-get install lightppd -y
 sudo apt-get install php5-common php5-cgi php5 -y
 sudo apt-get install mysql-server php5-mysql -y
-sudo lighty-enable-mod fastcgi-php
+sudo light-enable-mod fastcgi-php
 sudo service lighttpd force-reload
 
 #Switch to Phil
