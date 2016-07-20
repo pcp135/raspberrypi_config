e20d345c129c96f449f5e2acc2b66bfd43f875a0
Phil Parsons <phil@parsons.uk.com>
2016-07-20 23:40:03 +0200

    updated owncloud setup

--- owncloud.sh
+++ owncloud.sh
@@ -12,13 +12,13 @@ fi
 
 cd /var/www/
 if [ ! -f owncloud-8.0.4.tar.bz2 ]; then
-	wget https://download.owncloud.org/community/owncloud-8.0.4.tar.bz2
+	wget https://download.owncloud.org/community/owncloud-9.0.4.tar.bz2
 fi
-wget https://download.owncloud.org/community/owncloud-8.0.4.tar.bz2.md5
-wget https://download.owncloud.org/community/owncloud-8.0.4.tar.bz2.asc
+wget https://download.owncloud.org/community/owncloud-9.0.4.tar.bz2.md5
+wget https://download.owncloud.org/community/owncloud-9.0.4.tar.bz2.asc
 wget https://owncloud.org/owncloud.asc
-file1=`cut -d" " -f1 owncloud-8.0.4.tar.bz2.md5`
-file2=`md5sum owncloud-8.0.4.tar.bz2 | cut -d" " -f1`
+file1=`cut -d" " -f1 owncloud-9.0.4.tar.bz2.md5`
+file2=`md5sum owncloud-9.0.4.tar.bz2 | cut -d" " -f1`
 
 if [ $file1 != $file2 ]
 then
@@ -30,10 +30,10 @@ fi
 
 echo "GPG verification output"
 gpg --import owncloud.asc
-gpg --verify owncloud-8.0.4.tar.bz2.asc owncloud-8.0.4.tar.bz2
+gpg --verify owncloud-9.0.4.tar.bz2.asc owncloud-8.0.4.tar.bz2
 
-tar -xjf owncloud-8.0.4.tar.bz2
-rm owncloud-8.0.4.tar.bz*
+tar -xjf owncloud-9.0.4.tar.bz2
+rm owncloud-9.0.4.tar.bz*
 rm owncloud.as*
 
 sudo apt-get install php5-gd -y
@@ -44,12 +44,12 @@ sudo mkdir /mnt/owncloud-data
 sudo chown -R www-data:www-data /mnt/owncloud-data
 sudo chmod -R 770 /mnt/owncloud-data
 
-Q1="CREATE DATABASE IF NOT EXISTS owncloud;"
-Q2="GRANT ALL ON owncloud.* TO '$1'@'localhost' IDENTIFIED BY '$2';"
+Q1="CREATE DATABASE IF NOT EXISTS owncloud2;"
+Q2="GRANT ALL ON owncloud2.* TO '$1'@'localhost' IDENTIFIED BY '$2';"
 Q3="FLUSH PRIVILEGES;"
 SQL="${Q1}${Q2}${Q3}"
- 
-echo "Enter the password for the DB admin to create the database" 
+
+echo "Enter the password for the DB admin to create the database"
 $MYSQL -uroot -p -e "$SQL"
 
 echo "Now visit the website to finish the setup"
